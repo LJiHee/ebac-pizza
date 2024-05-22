9f4e90e7691e62eeef1eaa1cf62b2a737bb93d5d
LJiHee <leiryellen.2004@gmail.com>
2024-05-22 00:48:06 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+     $('#telefone').mask('(00) 00000-0000',
+          {
+               placeholder: '(00) 00000-0000'
+          })
+
+     $('form').validate({
+          rules: {
+               nome: {
+                    required: true
+               },
+               email: {
+                    required: true,
+                    email: true
+               },
+               telefone: {
+                    required: true
+               }
+          },
+          messages: {
+               nome: 'Por favor, insira o seu nome completo'
+          },
+          submitHandler: function (form) {
+               console.log(form)
+          },
+          invalidHandler: function (evento, validador) {
+               let camposIncorretos = validador.numberOfInvalids();
+               if (camposIncorretos) {
+                    alert(`Existem ${camposIncorretos} campos incorreto(s)`)
+               }
+          }
+     })
+})
